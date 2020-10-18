1af5754b1dc25984a2648c84b2d40da729b5de0c
Bryson Faggart <brysonwf@gmail.com>
2020-10-17 21:27:06 -0400

    a

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ var scriptsPath = '.';
 var folders = getFolders(scriptsPath);
 
 //stage  = lint all src files, break on error
-gulp.task('buildFunction-ci', done => {
+gulp.task('buildFunction-ci', function () {
   var diffUrl = process.env.CIRCLE_COMPARE_URL.split('/');
   var diffUrlLength = diffUrl.length;
   var diffHash = diffUrl[diffUrlLength - 1]
@@ -88,12 +88,12 @@ gulp.task('buildFunction-ci', done => {
           //   deployment: 1
           // }));
         } else {
+          done();
           return null;
         }
       });
     }));
   });
-  done();
 });
 
 gulp.task('buildFunction-local', function () {
