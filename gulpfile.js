be047cfe420c9aaf2e9110b1da4923c1c7e51353
Bryson Faggart <brysonwf@gmail.com>
2020-10-18 09:16:36 -0400

    revert

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ function getFiles(dir, files_) {
   return files_;
 }
 
-var scriptsPath = process.env.CIRCLE_WORKING_DIRECTORY;
+var scriptsPath = '.';
 var folders = getFolders(scriptsPath);
 
 //stage  = lint all src files, break on error
